6372e531b0b6b674ac5b882deb2e7d23f1822cc8
GangHyeon An <cccloud1016@gmail.com>
2024-12-03 12:31:30 +0900

    [Update] [DTO - Question] [question_chap -> sub_code]

--- project_403/src/main/java/com/spring/domain/Question.java
+++ project_403/src/main/java/com/spring/domain/Question.java
@@ -4,12 +4,13 @@ import org.springframework.web.multipart.MultipartFile;
 
 public class Question {
 	private int question_num;	//문제번호
+	private String question_inner;	//문제 내용
 	private String question_ans;	//정답
 	private MultipartFile question_img;	//문제 이미지
-	private String questuin_img_name;	//문제 이미지 이름
+	private String question_img_name;	//문제 이미지 이름
 	private int question_plus;	//문제풀이 추가 카운트
 	private int question_count;	//문제풀이 횟수
-	private String question_chap;	//문제분류
+	private int sub_code;	//문제코드
 	
 	//Getter(), Setter()
 	public int getQuestion_num() {
@@ -17,6 +18,12 @@ public class Question {
 	}
 	public void setQuestion_num(int question_num) {
 		this.question_num = question_num;
+	}
+	public String getQuestion_inner() {
+		return question_inner;
+	}
+	public void setQuestion_inner(String question_inner) {
+		this.question_inner = question_inner;
 	}
 	public String getQuestion_ans() {
 		return question_ans;
@@ -30,11 +37,11 @@ public class Question {
 	public void setQuestion_img(MultipartFile question_img) {
 		this.question_img = question_img;
 	}
-	public String getQuestuin_img_name() {
-		return questuin_img_name;
+	public String getQuestion_img_name() {
+		return question_img_name;
 	}
-	public void setQuestuin_img_name(String questuin_img_name) {
-		this.questuin_img_name = questuin_img_name;
+	public void setQuestion_img_name(String question_img_name) {
+		this.question_img_name = question_img_name;
 	}
 	public int getQuestion_plus() {
 		return question_plus;
@@ -48,13 +55,11 @@ public class Question {
 	public void setQuestion_count(int question_count) {
 		this.question_count = question_count;
 	}
-	public String getQuestion_chap() {
-		return question_chap;
+	public int getSub_code() {
+		return sub_code;
 	}
-	public void setQuestion_chap(String question_chap) {
-		this.question_chap = question_chap;
-	}
-	
-	
+	public void setSub_code(int sub_code) {
+		this.sub_code = sub_code;
+	}	
 	
 }
