394fe5443878ea392b8dc7adcdad04ddaf6b2ee9
GangHyeon An <cccloud1016@gmail.com>
2024-12-17 10:52:58 +0900

    [Update] [DAO - QnA_Repository] [addCommentRoot(), ++getCommentDepth()]

--- project_403/src/main/java/com/spring/repository/QnA_RepositoryImpl.java
+++ project_403/src/main/java/com/spring/repository/QnA_RepositoryImpl.java
@@ -20,23 +20,13 @@ public class QnA_RepositoryImpl implements QnA_Repository{
 		
 		Connection conn = null;
 		PreparedStatement pstmt = null;
-		ResultSet rs = null;
 		try {
 			//DB연결
 			conn = DBConnection.getConnection();
-			//쿼리전송(Read) - comment_root 값 설정
-			String SQL_SELECT = "SELECT MAX(comment_root) FROM QnA";
-			pstmt = conn.prepareStatement(SQL_SELECT);
-			rs = pstmt.executeQuery();
-			if(!rs.next()) {
-				qna.setComment_root(1);
-			}else {
-				qna.setComment_root(rs.getInt(1)+1);
-			}
-			System.out.println("리파지토리 | SELECT 성공 comment_root 값 설정 완료");
 			//쿼리전송(Create)
-			String SQL_INSERT = "INSERT INTO QnA VALUES(NULL, ?, ?, ?, 0, 0, ?, ?, ?, 0, 0)";
-			pstmt = conn.prepareStatement(SQL_INSERT);
+			qna.setComment_root(getCommentDepth("comment_root"));
+			String SQL = "INSERT INTO QnA VALUES(NULL, ?, ?, ?, 0, 0, ?, ?, ?, 0, 0)";
+			pstmt = conn.prepareStatement(SQL);
 			pstmt.setString(1, "NULL");
 			pstmt.setString(2, qna.getQuestion_serial());
 			pstmt.setInt(3, qna.getComment_root());
@@ -49,11 +39,42 @@ public class QnA_RepositoryImpl implements QnA_Repository{
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-			try {rs.close();} catch (SQLException e) {e.printStackTrace();}
 			try {pstmt.close();} catch (SQLException e) {e.printStackTrace();}
 			try {conn.close();} catch (SQLException e) {e.printStackTrace();}
 		}
 		
 	}
-
+	
+	//comment_root, comment_parent, comment_child의 최대값을 구하는 함수
+	private int getCommentDepth(String name) {
+		System.out.println("리파지토리 | getCommentDepth() 도착 "+name+"의 값 설정 시작");
+		int depth = 0;
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			//DB연결
+			conn = DBConnection.getConnection();;
+			//쿼리전송
+			String SQL = "SELECT MAX("+name+") FROM QnA";
+			pstmt = conn.prepareStatement(SQL);
+			rs = pstmt.executeQuery();
+			if(!rs.next()) {
+				depth = 1;
+			}else {
+				depth = rs.getInt(1)+1;
+			}
+			System.out.println("리파지토리 | SELECT 성공 "+name+"의 값을 "+depth+"으로 설정 완료");
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {rs.close();} catch (SQLException e) {e.printStackTrace();}
+			try {pstmt.close();} catch (SQLException e) {e.printStackTrace();}
+			try {conn.close();} catch (SQLException e) {e.printStackTrace();}
+		}
+		
+		return depth;
+	}
+	
 }
